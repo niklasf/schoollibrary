ba1f311d3d66a1682cd10f9e4d0616a6f6e73cf5
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2014-01-29 01:33:30 +0100

    Remove permisson debugging from auth.sh

--- auth.sh
+++ auth.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo "user"
-exit 0
-
 # Accept the user admin with password admin.
 HTPASSWD=etc/schoollibrary/htpasswd HTGROUPS=etc/schoollibrary/htgroups usr/share/schoollibrary/auth-htpasswd.pl $1 $2
